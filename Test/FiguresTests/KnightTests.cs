24681df4ce0ce25625b9ed99f222962729b6bcf2
Egor <mr.egorka.kardash.1999@gmail.com>
2021-06-14 00:44:27 +0300

    Add Knight Tests.

---
+++ Test/FiguresTests/KnightTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using Chess;
+using Chess.Enum;
+using Chess.Figures;
+
+namespace Test
+{
+    public class KnightTests
+    {
+        [Test]
+        public void CanMoveTo_WhenCoordinateIsCorrect_ShouldReturnTrue()
+        {
+            // Act
+            var knight = new Knight(Color.White);
+
+            var result = knight.CanMoveTo(7, 2, 5, 3);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void CanMoveTo_WhenCoordinateIsNotCorrect_ShouldReturnFalse()
+        {
+            // Act
+            var knight = new Knight(Color.White);
+
+            var result = knight.CanMoveTo(7, 2, 5, 4);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+    }
+}
