915df88923ee4036c4359913853930806aa9f433
Egor <mr.egorka.kardash.1999@gmail.com>
2021-06-14 00:44:07 +0300

    Add TestCases.

--- Test/FiguresTests/BishopTests.cs
+++ Test/FiguresTests/BishopTests.cs
@@ -9,13 +9,16 @@ namespace Test
 {
     public class BishopTests
     {
-        [Test]
-        public void CanMoveTo_WhenCoordinateIsCorrect_ShouldReturnTrue()
+        [TestCase(4, 4, 7, 1, TestName = "CanMoveTo_WhenBishopMoveDiagonallyLeftDown_ShouldReturnTrue")]
+        [TestCase(4, 4, 7, 7, TestName = "CanMoveTo_WhenBishopMoveDiagonallyRightDown_ShouldReturnTrue")]
+        [TestCase(4, 4, 0, 0, TestName = "CanMoveTo_WhenBishopMoveDiagonallyLeftUp_ShouldReturnTrue")]
+        [TestCase(4, 4, 1, 7, TestName = "CanMoveTo_WhenBishopMoveDiagonallyRightUp_ShouldReturnTrue")]
+        public void Test_CanMoveTo(int x1, int y1, int x2, int y2)
         {
             // Act
             var bishop = new Bishop(Color.White);
 
-            var result = bishop.CanMoveTo(7, 2, 5, 4);
+            var result = bishop.CanMoveTo(x1, y1, x2, y2);
 
             // Assert
             result.Should().BeTrue();
