9a55022f8a841217d717158b0f3ee274ee3f278d
Egor <mr.egorka.kardash.1999@gmail.com>
2021-06-14 00:43:30 +0300

    Override method ToString().

--- Chess/Point.cs
+++ Chess/Point.cs
@@ -15,5 +15,21 @@ namespace Chess
             Y = y;
         }
 
+        public override bool Equals(object obj)
+        {
+            return obj is Point point &&
+                   X == point.X &&
+                   Y == point.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"Coordinate X = {X} \n Coordinate Y = {Y}");
+        }
     }
 }
