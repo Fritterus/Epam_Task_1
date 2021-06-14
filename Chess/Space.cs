0e06cf8dc675630673fe4c076d4342c347772855
Egor <mr.egorka.kardash.1999@gmail.com>
2021-06-14 16:59:34 +0300

    Add comments.

--- Chess/Space.cs
+++ Chess/Space.cs
@@ -1,26 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Chess.Figures;
 
 namespace Chess
 {
+    /// <summary>
+    /// Class describing chessboard space(square)
+    /// </summary>
     internal class Space
     {
+        /// <summary>
+        /// Property X-coordinate for chessboard space(square)
+        /// </summary>
         public int X { get; set; }
+
+        /// <summary>
+        /// Property Y-coordinate for chessboard space(square)
+        /// </summary>
         public int Y { get; set; }
+
+        /// <summary>
+        /// Constructor initializes coordinates x and y for chessboard space
+        /// </summary>
+        /// <param name="x">x-coordinate chessboard space</param>
+        /// <param name="y">y-coordinate chessboard space</param>
         public Space(int x, int y)
         {
             X = x;
             Y = y;
         }
 
+        /// <summary>
+        /// Property figure
+        /// </summary>
+        public Figure Figure { get; set; }
 
+        /// <summary>
+        /// Method discribing rule of move for current figure 
+        /// </summary>
+        /// <param name="point">Target coordinate</param>
+        /// <returns>True, if current figure can move on target coordinate, otherwise - false</returns>
         public bool CanFigureMoveTo (Point point)
         {
             return Figure.CanMoveTo(X, Y, point.X, point.Y);
         }
 
+        /// <summary>
+        /// Method for finding path of current figure
+        /// </summary>
+        /// <param name="point">Target coordinate</param>
+        /// <returns>Collection of object of type Point(x, y)</returns>
         public IEnumerable<Point> GetFigureWay (Point point)
         {
             return Figure.Way(X, Y, point.X, point.Y);
@@ -38,8 +67,6 @@ namespace Chess
         {
             return HashCode.Combine(X, Y, Figure);
         }
-
-        public Figure Figure { get; set; }
 
         public override string ToString()
         {
