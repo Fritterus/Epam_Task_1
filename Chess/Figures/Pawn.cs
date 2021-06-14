c3af9f0449313a05962c9e90ee557cf496d06655
Egor <mr.egorka.kardash.1999@gmail.com>
2021-06-14 16:59:05 +0300

    Add comments.

--- Chess/Figures/Pawn.cs
+++ Chess/Figures/Pawn.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Chess.Enum;
 
 namespace Chess.Figures
 {
+    /// <summary>
+    /// Class describing behavior pawn figure
+    /// </summary>
     internal class Pawn : Figure
     {
+        /// <summary>
+        /// Constructor initializes color and type figure for pawn figure
+        /// </summary>
+        /// <param name="color">Figure color</param>
         public Pawn (Color color)
         {
             Color = color;
             Type = FigureType.Pawn;
         }
 
-
+        /// <summary>
+        /// Method discribing rule of move for pawn
+        /// </summary>
+        /// <param name="x1">Start X-coordinate </param>
+        /// <param name="y1">Start Y-coordinate</param>
+        /// <param name="x2">End X-coordinate</param>
+        /// <param name="y2">End Y-coordinate</param>
+        /// <returns>True, if coordinates correspond pawn's move, otherwise - false</returns>
         public override bool CanMoveTo(int x1, int y1, int x2, int y2)
         {
             if ((x2 - x1) == 1 && (y2 == y1) && Color == Color.Black)
@@ -27,13 +40,33 @@ namespace Chess.Figures
             return false;
         }
 
+        /// <summary>
+        /// Method discribing rule of move for pawn
+        /// </summary>
+        /// <param name="point1">Start coordinate</param>
+        /// <param name="point2">End coordinate</param>
+        /// <returns>True, if coordinates correspond pawn's move, otherwise - false</returns>
         public override bool CanMoveTo(Point point1, Point point2)
         {
-            //if (Math.Abs(X - point.X) == 2 && Math.Abs(Y - point.Y) == 1) return true;
-            //if (Math.Abs(X - point.X) == 1 && Math.Abs(Y - point.Y) == 2) return true;
+            if ((point2.X - point1.X) == 1 && (point2.Y == point1.Y) && Color == Color.Black)
+            {
+                return true;
+            }
+            if ((point2.X - point1.X) == -1 && (point2.Y == point1.Y) && Color == Color.White)
+            {
+                return true;
+            }
             return false;
         }
 
+        /// <summary>
+        /// Method discribing rule of attack for pawn
+        /// </summary>
+        /// <param name="x1">Start X-coordinate </param>
+        /// <param name="y1">Start Y-coordinate</param>
+        /// <param name="x2">End X-coordinate</param>
+        /// <param name="y2">End Y-coordinate</param>
+        /// <returns>True, if pawn can attack, otherwise - false</returns>
         public bool CanAttack(int x1, int y1, int x2, int y2)
         {
             if (Color == Color.White && (x2 - x1) == -1 && Math.Abs(y2 - y1) == 1)
@@ -48,6 +81,12 @@ namespace Chess.Figures
 
         }
 
+        /// <summary>
+        /// Method discribing rule of attack for pawn
+        /// </summary>
+        /// <param name="point1">Start coordinate</param>
+        /// <param name="point2">End coordinate</param>
+        /// <returns>True, if pawn can attack, otherwise - false</returns>
         public bool CanAttack(Point point1, Point point2)
         {
             if (Color == Color.White && (point2.X - point1.X) == -1 && Math.Abs(point2.Y - point1.Y) == 1)
@@ -62,14 +101,26 @@ namespace Chess.Figures
 
         }
 
-
-
+        /// <summary>
+        /// Method for finding path of pawn
+        /// </summary>
+        /// <param name="x1">Start X-coordinate </param>
+        /// <param name="y1">Start Y-coordinate</param>
+        /// <param name="x2">End X-coordinate</param>
+        /// <param name="y2">End Y-coordinate</param>
+        /// <returns>Collection of object of type Point(x, y)</returns>
         public override IEnumerable<Point> Way(int x1, int y1, int x2, int y2)
         {
             yield return new Point(x1, y1);
             yield return new Point(x2, y2);
         }
 
+        /// <summary>
+        /// Method for finding path of pawn
+        /// </summary>
+        /// <param name="point1">Start coordinate</param>
+        /// <param name="point2">End coordinate</param>
+        /// <returns>Collection of object of type Point(x, y)</returns>
         public override IEnumerable<Point> Way(Point point1, Point point2)
         {
             yield return new Point(point1.X, point1.Y);
