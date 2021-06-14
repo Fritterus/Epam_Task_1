c0559c613132022f44799033586b28a68c44e575
Egor <mr.egorka.kardash.1999@gmail.com>
2021-06-14 16:59:21 +0300

    Add comments.

--- Chess/Interfaces/IBoard.cs
+++ Chess/Interfaces/IBoard.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Chess.Interfaces
+﻿namespace Chess.Interfaces
 {
+    /// <summary>
+    /// Interface describing chessboard
+    /// </summary>
     interface IBoard
     {
+        /// <summary>
+        /// Method describing turn on the board
+        /// </summary>
+        /// <param name="x1">Start X-coordinate </param>
+        /// <param name="y1">Start Y-coordinate</param>
+        /// <param name="x2">End X-coordinate</param>
+        /// <param name="y2">End Y-coordinate</param>
         public void Turn(int x1, int y1, int x2, int y2);
+
+        /// <summary>
+        /// Method describing turn on the board
+        /// </summary>
+        /// <param name="point1">Start coordinate</param>
+        /// <param name="point2">End coordinate</param>
         public void Turn(Point point1, Point point2);
     }
 }
