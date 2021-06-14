9ab84a75521920c85aa2bd6f41a8a5699e5431e6
Egor <mr.egorka.kardash.1999@gmail.com>
2021-06-14 16:58:57 +0300

    Add comments.

--- Chess/Figures/King.cs
+++ Chess/Figures/King.cs
@@ -1,28 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Chess.Enum;
 
 namespace Chess.Figures
 {
+    /// <summary>
+    /// Class describing behavior king figure
+    /// </summary>
     internal class King : Figure
     {
+        /// <summary>
+        /// Constructor initializes color and type figure for king figure
+        /// </summary>
+        /// <param name="color">Figure color</param>
         public King(Color color)
         {
             Color = color;
             Type = FigureType.King;
         }
 
+        /// <summary>
+        /// Method discribing rule of move for king
+        /// </summary>
+        /// <param name="x1">Start X-coordinate </param>
+        /// <param name="y1">Start Y-coordinate</param>
+        /// <param name="x2">End X-coordinate</param>
+        /// <param name="y2">End Y-coordinate</param>
+        /// <returns>True, if coordinates correspond king's move, otherwise - false</returns>
         public override bool CanMoveTo(int x1, int y1, int x2, int y2)
         {
             if (Math.Abs(x2 - x1) <= 1 && Math.Abs(y2 - y1) <= 1) return true;
             return false;
         }
 
+        /// <summary>
+        /// Method discribing rule of move for king
+        /// </summary>
+        /// <param name="point1">Start coordinate</param>
+        /// <param name="point2">End coordinate</param>
+        /// <returns>True, if coordinates correspond king's move, otherwise - false</returns>
         public override bool CanMoveTo(Point point1, Point point2)
         {
             if (Math.Abs(point2.X - point1.X) <= 1 && Math.Abs(point2.Y - point1.Y) <= 1) return true;
             return false;
+        }
+
+        /// <summary>
+        /// Method for finding path of king
+        /// </summary>
+        /// <param name="x1">Start X-coordinate </param>
+        /// <param name="y1">Start Y-coordinate</param>
+        /// <param name="x2">End X-coordinate</param>
+        /// <param name="y2">End Y-coordinate</param>
+        /// <returns>Collection of object of type Point(x, y)</returns>
+        public override IEnumerable<Point> Way(int x1, int y1, int x2, int y2)
+        {
+            yield return new Point(x1, y1);
+            yield return new Point(x2, y2);
+        }
+
+        /// <summary>
+        /// Method for finding path of king
+        /// </summary>
+        /// <param name="point1">Start coordinate</param>
+        /// <param name="point2">End coordinate</param>
+        /// <returns>Collection of object of type Point(x, y)</returns>
+        public override IEnumerable<Point> Way(Point point1, Point point2)
+        {
+            yield return new Point(point1.X, point1.Y);
+            yield return new Point(point2.X, point2.Y);
         }
 
         public override bool Equals(object obj)
@@ -35,18 +81,6 @@ namespace Chess.Figures
         public override int GetHashCode()
         {
             return HashCode.Combine(Type, Color);
-        }
-
-        public override IEnumerable<Point> Way(int x1, int y1, int x2, int y2)
-        {
-            yield return new Point(x1, y1);
-            yield return new Point(x2, y2);
-        }
-
-        public override IEnumerable<Point> Way(Point point1, Point point2)
-        {
-            yield return new Point(point1.X, point1.Y);
-            yield return new Point(point2.X, point2.Y);
         }
     }
 }
